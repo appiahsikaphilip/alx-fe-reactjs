5b178d29899359a8ccb5489ab975627e0fb651c9
Appiah Sika Philip <appiahsikaphilip@gmail.com>
2025-11-07 01:13:55 +0000

    Added a bio to App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,13 @@
-// src/App.jsx
+// src/components/UserProfile.jsx
 
-import UserProfile from './components/UserProfile';
-
-function App() {
+const UserProfile = (props) => {
   return (
-    <>
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
-    </>
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
   );
-}
+};
 
-export default App;
+export default UserProfile;
