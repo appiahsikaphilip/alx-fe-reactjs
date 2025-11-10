88fbb955544e1cc6cf44f0485f2bbfc768bcf2e1
Appiah Sika Philip <appiahsikaphilip@gmail.com>
2025-11-10 22:57:25 +0000

    Refactoring  Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,14 @@
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Appiah Sika Philip", email: "sika@gmail.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
