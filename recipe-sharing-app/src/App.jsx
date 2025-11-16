d66bdd85895019307bfe9943844d6d856c62ff23
Appiah Sika Philip <appiahsikaphilip@gmail.com>
2025-11-16 05:19:06 +0000

    Checks for the installation of react-router-dom

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
-import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
 
-      <AddRecipeForm />
-
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
+
+      <AddRecipeForm />
     </div>
   );
 }
