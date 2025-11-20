c41a562195695a3a1bf93de3680b1676627d3ff5
Appiah Sika Philip <appiahsikaphilip@gmail.com>
2025-11-20 17:16:58 +0000

    Add Contextto App.jsx

--- alx-react-app-props-old/src/App.jsx
+++ alx-react-app-props-old/src/App.jsx
@@ -1,9 +1,22 @@
-import ProfilePage from './ProfilePage';
+// src/App.jsx
+import React from "react";
+import { UserProvider } from "./UserContext";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MainContent from "./components/MainContent";
+import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
-  return <ProfilePage userData={userData} />;
+  return (
+    <UserProvider>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <UserProfile />
+      <Footer />
+    </UserProvider>
+  );
 }
 
 export default App;
