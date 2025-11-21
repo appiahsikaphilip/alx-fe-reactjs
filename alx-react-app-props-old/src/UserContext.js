1569f753f42ce7b93628c04254bedaa82a272657
Philip Appiah Sika <appiahsikaphilip@gmail.com>
2025-11-21 02:22:39 +0000

    Delete alx-react-app-props-old directory

--- alx-react-app-props-old/src/UserContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// Create the context object
-// The default value (here, {}) is used when a component consumes the context 
-// but has no matching Provider above it.
-export const UserContext = React.createContext({});
