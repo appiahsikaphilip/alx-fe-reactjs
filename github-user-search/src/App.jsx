9b32082dc1190b3979dbc23211635bfb1406bf66
Appiah Sika Philip <appiahsikaphilip@gmail.com>
2025-11-22 23:29:06 +0000

    Setup GitHub user search project with Vite and Axios

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,9 @@
+import React from "react";
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Start building your search component here...</p>
     </div>
   );
 }
