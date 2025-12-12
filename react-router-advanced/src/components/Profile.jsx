207c0b779cb9f8b5e6b555e3887bd9ffc1b132fe
Appiah Sika Philip <appiahsikaphilip@gmail.com>
2025-12-12 05:40:58 +0000

    Advanced Routing in React with React Router

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+
+      <nav>
+        <ul>
+          <li><Link to="details">Profile Details</Link></li>
+          <li><Link to="settings">Profile Settings</Link></li>
+        </ul>
+      </nav>
+
+      {/* Required for nested routes */}
+      <Outlet />
+    </div>
+  );
+}
+
+export default Profile;
