49ae854f2b2db9d197112f90ab9c44899ef76b23
Appiah Sika Philip <appiahsikaphilip@gmail.com>
2025-12-13 06:11:10 +0000

    Implement nested profile routes

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,26 +1,18 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProfileDetails from "./ProfileDetails";
 import ProfileSettings from "./ProfileSettings";
 
-function Profile() {
+const Profile = () => {
   return (
     <div>
-      <h2>Profile Page</h2>
+      <h2>Profile</h2>
 
-      <nav>
-        <ul>
-          <li><Link to="details">Profile Details</Link></li>
-          <li><Link to="settings">Profile Settings</Link></li>
-        </ul>
-      </nav>
-
-      {/* NESTED ROUTES INSIDE THE COMPONENT */}
       <Routes>
-        <Route path="details" element={<ProfileDetails />} />
+        <Route path="/" element={<ProfileDetails />} />
         <Route path="settings" element={<ProfileSettings />} />
       </Routes>
     </div>
   );
-}
+};
 
 export default Profile;
